03b119e0ef724f85546f69229f4416cc6517cc8a
Mr_Yang <1512743313@qq.com>
2019-06-17 11:30:10 +0800

    XLin

--- 超市管理系统/__init__.cs
+++ 超市管理系统/__init__.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BLL;
 
 namespace 超市管理系统
 {
@@ -20,6 +21,12 @@ namespace 超市管理系统
         private void Btn_Close_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void __init___Load(object sender, EventArgs e)
+        {
+            string checkCode = MyCode.GetRandomCode(4);//获取4个随机的数字或字母
+           image_Code.Image = MyCode.CreateImage(checkCode);//实现验证码图片
         }
     }
 }
