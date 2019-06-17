38545bb55825ef8d01774cb6f9e3ec19737e000e
Mr_Yang <1512743313@qq.com>
2019-06-17 10:36:22 +0800

    1.0.8

--- 超市管理系统/__init__.Designer.cs
+++ 超市管理系统/__init__.Designer.cs
@@ -28,22 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.Location = new System.Drawing.Point(327, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "卡号：";
             // 
             // __init__
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::超市管理系统.Properties.Resources.登陆背景图;
-            this.ClientSize = new System.Drawing.Size(971, 437);
+            this.ClientSize = new System.Drawing.Size(734, 352);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "__init__";
             this.Text = "__init__";
             this.TransparencyKey = System.Drawing.Color.Black;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
